49a3c8efcc56baa78770ae84d90b5d196191c9fa
QueenGytha <queengytha.clop@gmail.com>
2025-06-20 15:34:37 +1200

    popout

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ import { bind_setting, bind_function, set_setting_ui_element } from './uiBinding
 import { refresh_character_select, refresh_select2_element } from './characterSelect.js';
 import { update_save_icon_highlight, update_profile_section, update_preset_dropdown, update_combined_summary_preset_dropdown, update_connection_profile_dropdown, refresh_settings, update_error_detection_preset_dropdown } from './profileUI.js';
 import { set_data, get_data, get_memory, edit_memory, clear_memory, toggle_memory_value, get_previous_swipe_memory, remember_message_toggle, forget_message_toggle, get_character_key } from './messageData.js';
+import { initialize_popout, open_popout, close_popout, toggle_popout } from './popout.js';
 
 // THe module name modifies where settings are stored, where information is stored on message objects, macros, etc.
 const MODULE_NAME = 'auto_summarize_memory';
@@ -1398,80 +1399,7 @@ function initialize_menu_buttons() {
 }
 
 
-// Popout handling.
-// We save a jQuery reference to the entire settings content, and move it between the original location and the popout.
-// This is done carefully to preserve all event listeners when moving, and the move is always done before calling remove() on the popout.
-// clone() doesn't work because of the select2 widget for some reason.
-let $settings_element = null;  // all settings content
-let $original_settings_parent = null;  // original location of the settings element
-let $popout = null;  // the popout element
-let POPOUT_VISIBLE = false;
-function initialize_popout() {
-    // initialize the popout logic, creating the $popout object and storing the $settings_element
 
-    // Get the settings element and store it
-    $settings_element = $(`#${settings_div_id}`).find(`.inline-drawer-content .${settings_content_class}`)
-    $original_settings_parent = $settings_element.parent()  // where the settings are originally placed
-
-    debug('Creating popout window...');
-
-    // repurposes the zoomed avatar template (it's a floating div to the left of the chat)
-    $popout = $($('#zoomed_avatar_template').html());
-    $popout.attr('id', 'qmExtensionPopout').removeClass('zoomed_avatar').addClass('draggable').empty();
-
-    // create the control bar with the close button
-    const controlBarHtml = `<div class="panelControlBar flex-container">
-    <div class="fa-solid fa-grip drag-grabber hoverglow"></div>
-    <div class="fa-solid fa-circle-xmark hoverglow dragClose"></div>
-    </div>`;
-    $popout.append(controlBarHtml)
-
-    loadMovingUIState();
-    dragElement($popout);
-
-    // set up the popout button in the settings to toggle it
-    bind_function('#auto_summarize_popout_button', (e) => {
-        toggle_popout();
-        e.stopPropagation();
-    })
-
-    // when escape is pressed, toggle the popout.
-    // This has to be here because ST removes .draggable items when escape is pressed, destroying the popout.
-    $(document).on('keydown', async function (event) {
-         if (event.key === 'Escape') {
-             close_popout()
-         }
-    });
-}
-function open_popout() {
-    debug("Showing popout")
-    $('body').append($popout);  // add the popout to the body
-
-    // setup listener for close button to remove the popout
-    $popout.find('.dragClose').off('click').on('click', function () {
-        close_popout()
-    });
-
-    $settings_element.appendTo($popout)  // move the settings to the popout
-    $popout.fadeIn(animation_duration);
-    POPOUT_VISIBLE = true
-}
-function close_popout() {
-    debug("Hiding popout")
-    $popout.fadeOut(animation_duration, () => {
-        $settings_element.appendTo($original_settings_parent)  // move the settings back
-        $popout.remove()  // remove the popout
-    });
-    POPOUT_VISIBLE = false
-}
-function toggle_popout() {
-    // toggle the popout window
-    if (POPOUT_VISIBLE) {
-        close_popout()
-    } else {
-        open_popout()
-    }
-}
 
 // Entry point
 let memoryEditInterface;
@@ -1670,6 +1598,10 @@ export {
     get_connection_profiles,
     get_presets,
     verify_connection_profile,
+    animation_duration,
+    loadMovingUIState,
+    dragElement,
+    settings_div_id,
     //settingsManager
     saveSettingsDebounced,
     settings_ui_map
@@ -1691,3 +1623,4 @@ export * from './uiBindings.js';
 export * from './characterSelect.js';
 export * from './profileUI.js';
 export * from './messageData.js';
+export * from './popout.js';
